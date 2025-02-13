29a9e7dbbeb30f40988b73e71b7fbd7de8cf49c2
Kandy-Hamisi <hkandy2000@gmail.com>
2025-02-13 09:38:56 +0300

    Implemented adding and fetching subcription controller

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.post("/", (req, res) => {
-    res.send({ title: "Add new Subscription" });
-})
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.get("/", (req, res) => {
-    res.send({ title: "Get All Subscriptions" });
+  res.send({ title: "Get All Subscriptions" });
 });
+
+subscriptionRouter.get("/:id", authorize, getUserSubscriptions);
 
 export default subscriptionRouter;
